3830903d8ac23c8fb1791710854500d19c4de04c
waiphyooo12 <135409053+waiphyooo12@users.noreply.github.com>
2025-02-18 16:29:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gif = qs(".gif");
 const [yesBtn, noBtn] = [".yes-btn", ".no-btn"].map(qs);
 
 const handleYesClick = () => {
-  question.innerHTML = "Happy Valentine day KoKo!!!!!!";
+  question.innerHTML = "Happy Valentine day!!!!!!";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 
   // Remove the 'mouseover' event listener from noBtn
